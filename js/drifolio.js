0df565b8c636b0a1db1277324659c4d6dd5dcc73
RogelioDaniel <rogelio-daniel@hotmail.es>
2022-12-30 15:09:13 -0600

    Cambio de background

--- js/drifolio.js
+++ js/drifolio.js
@@ -2,14 +2,10 @@
 //========================
 //PRELOADER
 //========================
-$(window).load(function () { // makes sure the whole site is loaded
-    $('#status').fadeOut(); // will first fade out the loading animation
-    $('#preloader').delay(450).fadeOut('slow');
-    // will fade out the white DIV that covers the website.
-    $('body').delay(550).css({
-        'overflow': 'visible'
-    });
-})
+$(window).on("load",function(){
+    $("#preloader").fadeOut(1000);
+  })
+
 //========================
 //CUSTOM SCROLLBAR
 //========================
