8b7066db67889a4f43de05796cc55345416234c8
RogelioDaniel <rogelio-daniel@hotmail.es>
2023-01-18 15:09:15 -0600

    Cambio de imagenes a CDN

--- js/carouselportfolio.js
+++ js/carouselportfolio.js
@@ -1,4 +1,4 @@
-var radius = 200; // how big of the radius
+var radius = 240; // how big of the radius
 var autoRotate = true; // auto rotate or not
 var rotateSpeed = -60; // unit: seconds/360 degrees
 var imgWidth = 120; // width of images (unit: px)
