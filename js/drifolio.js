69ced74c6edc14b775b104c4077ab3b8b60abdcb
RogelioDaniel <rogelio-daniel@hotmail.es>
2023-09-18 10:19:36 -0600

    Add Catalogo experiencias

--- js/drifolio.js
+++ js/drifolio.js
@@ -2,9 +2,10 @@
 //========================
 //PRELOADER
 //========================
-$(window).on("load",function(){
-    $("#preloader").fadeOut(1000);
-  })
+setTimeout(function(){
+  $('.loader-bg').fadeToggle();
+
+}, 1500);
 
 //========================
 //CUSTOM SCROLLBAR
@@ -16,7 +17,59 @@ $(window).on("load",function(){
     cursorborderradius: "10px",
     cursorborder: "none",
 }); */
+// JavaScript Document
+// Claudio Gomboli . the EGGS LAB
+// 2012 / 8 / 29
+// Responsive animated gallery
 
+$('.portfolio').each(function(index)
+{
+    $(this).attr('id', 'img' + (index + 1));
+});
+    
+$('.portfolio').each(function(){
+  $('#navi').append('<div class="circle"></div>');
+});
+  
+$('.circle').each(function(index)
+{
+    $(this).attr('id', 'circle' + (index + 1));
+});   
+   
+$('.portfolio').click(function(){
+if($(this).hasClass('opened')){
+    $(this).removeClass('opened');
+    $(".portfolio").fadeIn("fast");
+    $(this).find('.ombra').fadeOut();
+    $("#navi div").removeClass("activenav");
+}
+else{
+	var indexi = $("#maincontent .portfolio").index(this) + 1;
+    $(this).addClass('opened'); 
+    $(".portfolio").not(this).fadeOut("fast");
+    $(this).find('.ombra').fadeIn();
+    $("#circle" + indexi).addClass('activenav'); 
+}
+});	
+
+//navi buttons
+$("#navi div").click(function() {
+if($(this).hasClass("activenav")){
+	return false;
+}
+		
+	$("#navi div").removeClass("activenav");
+	$(".portfolio").removeClass('opened');
+	$(".portfolio").show();
+        $('.ombra').hide();
+		
+	var index = $("#navi div").index(this) + 1;
+	$("#img" + index).addClass('opened'); 
+    $(".portfolio").not("#img" + index).fadeOut("fast");
+    $("#img" + index).find('.ombra').fadeIn();
+        
+    $(this).addClass("activenav");
+});
 
 //========================
 //SMOOTHSCROLL
